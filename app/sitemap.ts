924ca796b76bec4889f3c6767b48723200445fdf
Tioluwanimi Adeagbo Ayomide <159074912+Tioluwanim@users.noreply.github.com>
2026-07-09 20:29:41 +0100

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.radet.me",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.radet.me/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.radet.me/projects",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.radet.me/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+  ];
+}
