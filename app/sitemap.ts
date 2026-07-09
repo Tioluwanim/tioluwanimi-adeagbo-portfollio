ad5f18b25f32a2c0aedc8303218650cc5455e3a1
Tioluwanimi Adeagbo Ayomide <159074912+Tioluwanim@users.noreply.github.com>
2026-07-09 20:30:43 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,19 +9,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://www.radet.me/about",
+      url: "https://www.radet.me/#about",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://www.radet.me/projects",
+      url: "https://www.radet.me/#projects",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.9,
     },
     {
-      url: "https://www.radet.me/contact",
+      url: "https://www.radet.me/#contact",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
