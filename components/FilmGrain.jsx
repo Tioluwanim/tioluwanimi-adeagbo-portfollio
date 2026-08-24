cd3de729244b9da088270760d964ffc8a044bd1e
Tioluwanimi Adeagbo Ayomide <159074912+Tioluwanim@users.noreply.github.com>
2026-08-24 19:04:53 +0100

    Add FilmGrain component with SVG filter

---
+++ components/FilmGrain.jsx
@@ -0,0 +1,14 @@
+export default function FilmGrain() {
+  return (
+    <>
+      <svg className="film-grain" xmlns="http://www.w3.org/2000/svg">
+        <filter id="grain-filter">
+          <feTurbulence type="fractalNoise" baseFrequency="0.85" numOctaves="3" stitchTiles="stitch" />
+          <feColorMatrix type="saturate" values="0" />
+        </filter>
+        <rect width="100%" height="100%" filter="url(#grain-filter)" />
+      </svg>
+      <div className="vignette" />
+    </>
+  )
+}
